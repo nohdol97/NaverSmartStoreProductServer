84cb905c6adbd5efd656cdbd84ac59739aca2c3b
nohdol97 <nohdol97@gmail.com>
2024-07-09 21:12:00 +0900

    컬럼 수정

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask, request, jsonify
 from productManager import get_requested_products
 from proxy import get_shuffled_proxies
 from scheduler import start_scheduler
-import excelManager
-import os
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -24,51 +22,6 @@ def get_ip():
         return jsonify({"proxies": proxies})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-    
-@app.route('/updateExcel', methods=['POST'])
-def update_excel():
-    try:
-        if 'file' not in request.files:
-            return jsonify({'message': 'No file part'}), 400
-
-        file = request.files['file']
-        if file.filename == '':
-            return jsonify({'message': 'No selected file'}), 400
-
-        if file and file.filename.endswith('.xlsx'):
-            existing_file_path = 'data.xlsx'
-            new_file_path = 'uploaded_data.xlsx'
-
-            excelManager.download_excel_from_firebase(existing_file_path)
-            
-            try:
-                file.save(new_file_path)
-                # 파일 유효성 검사
-                excelManager.load_workbook(new_file_path)
-            except Exception as e:
-                if os.path.exists(new_file_path):
-                    os.remove(new_file_path)
-                return jsonify({'message': f'Invalid Excel file: {e}'}), 400
-
-            success, message = excelManager.process_and_update_excel(existing_file_path, new_file_path)
-
-            if not success:
-                os.remove(new_file_path)
-                os.remove(existing_file_path)
-                return jsonify({'message': message}), 400
-
-            excelManager.upload_excel_to_firebase(existing_file_path)
-
-            # Cleanup
-            os.remove(new_file_path)
-            os.remove(existing_file_path)
-
-            return jsonify({'message': message}), 200
-
-        return jsonify({'message': 'Invalid file format'}), 400
-    except Exception as e:
-        return jsonify({'message': str(e)}), 500
-
 if __name__ == '__main__':
     start_scheduler()
     app.run(host='0.0.0.0', port=5000)
--- scheduler.py
+++ scheduler.py
@@ -10,8 +10,8 @@ def run_scheduler():
         time.sleep(1)
 
 def start_scheduler():
-    # 23시 35분에 data.xlsx 파일 다운로드
-    schedule.every().day.at("23:35").do(download_file_from_firebase, 'data.xlsx')
+    # 22시 00분에 data.xlsx 파일 다운로드
+    schedule.every().day.at("22:00").do(download_file_from_firebase, 'data.xlsx')
     
     # 23시 40분에 get_product 실행
     schedule.every().day.at("23:40").do(get_product)
