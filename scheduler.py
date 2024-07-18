99aa8beedcfcab0e9177aa473f6cfd7e55c2ea78
nohdol97 <nohdol97@gmail.com>
2024-07-18 22:15:54 +0900

    엑셀 다운로드 시간, product 셋팅 시간 변경

--- scheduler.py
+++ scheduler.py
@@ -8,16 +8,16 @@ from firebaseManager import download_file_from_firebase
 def run_scheduler(stop_event):
     while not stop_event.is_set():
         now = datetime.now()
-        # 22:00:00에 data.xlsx 파일 다운로드
-        if now.hour == 22 and now.minute == 0 and now.second == 0:
+        # 23:00:00에 data.xlsx 파일 다운로드
+        if now.hour == 23 and now.minute == 0 and now.second == 0:
             try:
                 download_file_from_firebase('data.xlsx')
             except Exception as e:
                 print(f"Exception in download_file_from_firebase: {e}")
             time.sleep(1)  # 1초 대기하여 중복 실행 방지
         
-        # 23:00:00에 get_product 실행
-        if now.hour == 23 and now.minute == 0 and now.second == 0:
+        # 23:30:00에 get_product 실행
+        if now.hour == 23 and now.minute == 30 and now.second == 0:
             try:
                 get_product()
             except Exception as e:
